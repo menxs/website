8b245d41cd5d53eeeea80cd430a41dc0de7c29fa
Bryan Paxton <bryan@starbelly.io>
2021-03-22 16:48:24 -0500

    fix version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Erlef.MixProject do
     [
       app: :erlef,
       version: "0.1.0",
-      elixir: "~> 1.84",
+      elixir: "~> 1.8.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
